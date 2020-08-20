fcba79b2a744f8375156e98e6b97e57ef25c2d20
gitkp11 <patilkrunal11.kp@gmail.com>
2020-08-20 16:26:49 +0530

    Basic home app configured

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def home(request):
+    template = loader.get_template('home/index.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
