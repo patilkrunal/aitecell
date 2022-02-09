7b12cd3cf492cfdcc81fc7ef0c6ca50c75a15f83
patilkrunal <patilkrunal11.kp@gmail.com>
2022-02-09 23:36:00 +0530

    settings changed

---
+++ aitecell/settings/prod.py
@@ -0,0 +1,28 @@
+import os
+import dj_database_url
+from dotenv import load_dotenv
+from pathlib import Path
+
+from .base import *
+
+load_dotenv()
+
+DEBUG = False
+print("DEBUG", DEBUG)
+
+ALLOWED_HOSTS = ["*", "127.0.0.1", "localhost", "aitecell.herokuapp.com"']
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    "default": dj_database_url.config(default=os.getenv("POSTGRES_URI"))
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "https://kp.gtsb.io",
+    "https://kpstaging.gatsbyjs.io",
+)
